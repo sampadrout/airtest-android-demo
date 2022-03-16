a8e61f284f556149265286c8c0ca69b6fa034a04
sampadrout <sampad.rout@gmail.com>
2022-03-16 23:49:02 +0530

    files updated

--- AppiumTest.py
+++ AppiumTest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
 
 from airtest.core.api import *
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
@@ -24,17 +25,17 @@ class AppiumTest(unittest.TestCase):
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", self.dc)
 
     def testFirstAutomation(self):
-        if len(self.driver.find_elements_by_xpath("//*[@text='OK']")) > 0:
-            self.driver.find_element_by_xpath("//*[@text='OK']").click();
+        if len(self.driver.find_elements(MobileBy.XPATH, "//*[@text='OK']")) > 0:
+            self.driver.find_element(MobileBy.XPATH, "//*[@text='OK']").click();
         # Find location of Elements and perform action.
-        self.driver.find_element_by_xpath("//*[@text='Username']").send_keys('company')
-        self.driver.find_element_by_xpath("//*[@text='Password']").send_keys('company')
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='Username']").send_keys('company')
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='Password']").send_keys('company')
         #init_device("Android")
-        connect_device("Android://localhost:4723/emulator-5554")
+        connect_device("Android:///")
         # auto_setup(__file__, devices=["android://0.0.0.0:4723/emulator-5554"])
         touch(Template(r"tpl1647193221237.png", record_pos=(-0.001, -0.214), resolution=(1080, 2280)))
         # self.driver.find_element_by_xpath("//*[@text='Login']").click()
-        self.driver.find_element_by_xpath("//*[@text='Logout']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='Logout']").click()
 
     def tearDown(self):
         self.driver.quit()
