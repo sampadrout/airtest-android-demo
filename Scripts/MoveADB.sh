5cb7dabf3b9db392a37c1273959c4e1d9cb72470
sampadrout <sampad.rout@gmail.com>
2022-03-17 23:28:18 +0530

    files added

---
+++ Scripts/MoveADB.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /Users/runner/Library/Android/sdk/platform-tools
+mv adb adb.bkp
+ln -s /Users/runner/hostedtoolcache/Python/3.9.10/x64/lib/python3.9/site-packages/airtest/core/android/static/adb/mac/adb adb
